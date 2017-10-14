40e68731378ee1638632150df0f66737c546c47c
Galder Zamarreño <galder@zamarreno.com>
2017-10-14 20:00:23 +0200

    Convert train position to pojo

--- datagrid-feeder/src/main/java/streaming/datagrid/feeder/DatagridFeeder.java
+++ datagrid-feeder/src/main/java/streaming/datagrid/feeder/DatagridFeeder.java
@@ -4,6 +4,9 @@ import io.vertx.rxjava.core.AbstractVerticle;
 import io.vertx.rxjava.core.Vertx;
 import io.vertx.rxjava.core.http.HttpClient;
 import io.vertx.rxjava.core.http.HttpClientRequest;
+import streaming.data.model.GeoLocation;
+import streaming.data.model.Train;
+import streaming.data.model.TrainPosition;
 
 import static io.vertx.core.http.HttpMethod.GET;
 
@@ -26,11 +29,31 @@ public class DatagridFeeder extends AbstractVerticle {
             }
             return resp.toObservable();
          })
-         .subscribe(data ->
-            System.out.println("Server content " + data.toString("UTF-8")));
+         .subscribe(data -> {
+            String[] lines = data.toString("UTF-8").split("\\n");
+            for (String line : lines) {
+               if (!line.startsWith("train_id")) {
+                  String[] s = line.split("\\t");
+                  Train train = extractTrain(s);
+                  GeoLocation pos = extractPosition(s);
+                  // TODO: Add delay;
+                  TrainPosition trainPosition = new TrainPosition(train, pos, 0);
+                  System.out.println(trainPosition);
+               }
+            }
+         });
 
       // End request
       req.end();
+   }
+
+   private GeoLocation extractPosition(String[] s) {
+      return new GeoLocation(Double.valueOf(s[4]), Double.valueOf(s[5]),
+         s[6].isEmpty() ? 0 : Double.valueOf(s[6]));
+   }
+
+   private Train extractTrain(String[] s) {
+      return Train.make(s[0], s[1], s[2], s[3]);
    }
 
    public static void main(String[] args) {
