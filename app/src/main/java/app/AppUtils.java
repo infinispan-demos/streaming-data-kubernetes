2902c0db8a37f1722e51e5243a3dc2378cb8266f
Galder Zamarreño <galder@zamarreno.com>
2017-10-17 11:54:55 +0200

    Remove code for live coding

--- app/src/main/java/app/AppUtils.java
+++ app/src/main/java/app/AppUtils.java
@@ -3,15 +3,33 @@ package app;
 import app.model.Station;
 import app.model.Stop;
 import app.model.Train;
+import io.vertx.core.Handler;
+import io.vertx.core.Vertx;
+import io.vertx.ext.web.RoutingContext;
+import io.vertx.ext.web.handler.sockjs.BridgeEventType;
+import io.vertx.ext.web.handler.sockjs.BridgeOptions;
+import io.vertx.ext.web.handler.sockjs.PermittedOptions;
+import io.vertx.ext.web.handler.sockjs.SockJSHandler;
+import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 import org.infinispan.client.hotrod.marshall.ProtoStreamMarshaller;
 import org.infinispan.protostream.FileDescriptorSource;
 import org.infinispan.protostream.SerializationContext;
+import org.infinispan.query.remote.client.ProtobufMetadataManagerConstants;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.util.logging.Logger;
+
+import static org.infinispan.query.remote.client.ProtobufMetadataManagerConstants.PROTOBUF_METADATA_CACHE_NAME;
 
 public class AppUtils {
+
+  static final Logger log = Logger.getLogger(AppUtils.class.getName());
 
   static RemoteCacheManager createRemoteCacheManager() {
     RemoteCacheManager client = new RemoteCacheManager(
@@ -27,8 +45,51 @@ public class AppUtils {
       ctx.registerMarshaller(new Stop.Marshaller());
       ctx.registerMarshaller(new Station.Marshaller());
       ctx.registerMarshaller(new Train.Marshaller());
+      addModelToServer(client);
       return client;
     } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private static void addModelToServer(RemoteCacheManager client) {
+    InputStream is = AppUtils.class.getResourceAsStream("/app-model.proto");
+    RemoteCache<String, String> metaCache = client.getCache(PROTOBUF_METADATA_CACHE_NAME);
+    metaCache.put("app-model.proto", readInputStream(is));
+
+    String errors = metaCache.get(ProtobufMetadataManagerConstants.ERRORS_KEY_SUFFIX);
+    if (errors != null)
+      throw new RuntimeException("Error in proto file");
+  }
+
+  static Handler<RoutingContext> sockJSHandler(Vertx vertx) {
+    SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
+    PermittedOptions outPermit = new PermittedOptions().setAddress("delayed-trains");
+    BridgeOptions options = new BridgeOptions().addOutboundPermitted(outPermit);
+    sockJSHandler.bridge(options, be -> {
+      if (be.type() == BridgeEventType.REGISTER)
+        log.info("SockJs: client connected");
+
+      be.complete(true);
+    });
+    return sockJSHandler;
+  }
+
+  private static String readInputStream(InputStream is) {
+    try {
+      try {
+        final Reader reader = new InputStreamReader(is, "UTF-8");
+        StringWriter writer = new StringWriter();
+        char[] buf = new char[1024];
+        int len;
+        while ((len = reader.read(buf)) != -1) {
+          writer.write(buf, 0, len);
+        }
+        return writer.toString();
+      } finally {
+        is.close();
+      }
+    } catch (Exception e) {
       throw new RuntimeException(e);
     }
   }
--- app/src/main/java/app/Main.java
+++ app/src/main/java/app/Main.java
@@ -1,82 +1,13 @@
 package app;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Future;
-import io.vertx.core.Handler;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
-import io.vertx.ext.web.handler.sockjs.BridgeEventType;
-import io.vertx.ext.web.handler.sockjs.BridgeOptions;
-import io.vertx.ext.web.handler.sockjs.PermittedOptions;
-import io.vertx.ext.web.handler.sockjs.SockJSHandler;
-import org.infinispan.client.hotrod.RemoteCacheManager;
-import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
-
-import java.net.SocketAddress;
-import java.util.Set;
-import java.util.logging.Logger;
-
-import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 public class Main extends AbstractVerticle {
 
-  static final Logger log = Logger.getLogger(Main.class.getName());
-
   @Override
   public void start(Future<Void> startFuture) throws Exception {
-    Router router = Router.router(vertx);
-    router.get("/test").handler(this::test);
-    router.route("/eventbus/*").handler(this.sockJSHandler());
-
-    vertx.createHttpServer()
-      .requestHandler(router::accept)
-      .listen(8080, ar -> {
-        if (ar.succeeded())
-          System.out.println("Server started");
-
-        startFuture.handle(ar.mapEmpty());
-      });
-
-    vertx.deployVerticle(Injector.class.getName(), new DeploymentOptions());
-    vertx.deployVerticle(Listener.class.getName(), new DeploymentOptions());
-  }
-
-  private Handler<RoutingContext> sockJSHandler() {
-    SockJSHandler sockJSHandler = SockJSHandler.create(vertx);
-    PermittedOptions outPermit = new PermittedOptions().setAddress("delayed-trains");
-    BridgeOptions options = new BridgeOptions().addOutboundPermitted(outPermit);
-    sockJSHandler.bridge(options, be -> {
-      if (be.type() == BridgeEventType.REGISTER)
-        log.info("SockJs: client connected");
-
-      be.complete(true);
-    });
-    return sockJSHandler;
-  }
-
-  private void test(RoutingContext ctx) {
-    RemoteCacheManager client = new RemoteCacheManager(
-      new ConfigurationBuilder().addServer()
-        .host("datagrid-hotrod")
-        .port(11222).build());
-
-    client.getCache("repl").put("hello", "world");
-    Object value = client.getCache("repl").get("hello");
-
-    Set<SocketAddress> topology =
-      client.getCache("repl").getCacheTopologyInfo().getSegmentsPerServer().keySet();
-
-    JsonObject rsp = new JsonObject()
-      .put("get", value)
-      .put("topology", topology.toString());
-
-    ctx.response()
-        .putHeader(CONTENT_TYPE, "application/json; charset=utf-8")
-        .end(rsp.encodePrettily());
-
-    client.stop();
+    // TODO: live code
   }
 
 }
