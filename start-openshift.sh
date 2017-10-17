af31ac1897acf6c94930b5209f0e085f41392695
Galder Zamarreño <galder@zamarreno.com>
2017-10-17 08:53:56 +0200

    Bring OpenShift templates and make startup script clearer

--- start-openshift.sh
+++ start-openshift.sh
@@ -2,10 +2,16 @@
 
 set -e -x
 
-(cd $TMPDIR
-rm -drf infinispan-openshift-templates
-git clone https://github.com/infinispan/infinispan-openshift-templates
-cd infinispan-openshift-templates
-git checkout streaming_data_workshop
-make stop-openshift
-make start-openshift-with-catalog install-templates)
+oc cluster down
+oc cluster up --service-catalog
+
+oc login -u system:admin
+oc adm policy add-cluster-role-to-user cluster-admin developer
+
+oc login -u developer -p developer
+oc project openshift
+oc adm policy add-cluster-role-to-group system:openshift:templateservicebroker-client system:unauthenticated system:authenticated
+oc create -f openshift/infinispan-centos7-imagestream.json || true
+oc create -f openshift/infinispan-persistent-template.json || true
+oc create -f openshift/infinispan-ephemeral-template.json || true
+oc import-image is/infinispan || true
