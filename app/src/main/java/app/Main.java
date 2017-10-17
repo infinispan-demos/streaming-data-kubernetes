db2b7a572764acdc4418a925958f98d5ab4cddf5
Galder Zamarreño <galder@zamarreno.com>
2017-10-17 12:18:09 +0200

    Add logger for ease of use

--- app/src/main/java/app/Main.java
+++ app/src/main/java/app/Main.java
@@ -3,7 +3,11 @@ package app;
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Future;
 
+import java.util.logging.Logger;
+
 public class Main extends AbstractVerticle {
+
+  static final Logger log = Logger.getLogger(Main.class.getName());
 
   @Override
   public void start(Future<Void> startFuture) throws Exception {
