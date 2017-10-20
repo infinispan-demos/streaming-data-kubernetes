75405f9e7df537a0f891710b0dc63dc13c00139b
Galder Zamarreño <galder@zamarreno.com>
2017-10-20 16:50:18 +0200

    Add test script to quickly load up things

---
+++ test.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e -x
+
+./start-openshift.sh
+
+APP=datagrid
+USR=developer
+PASS=developer
+NUM_NODES=3
+NS=myproject
+
+oc project ${NS}
+
+# Create datagrid
+oc process -n openshift infinispan-ephemeral -p \
+  NUMBER_OF_INSTANCES=${NUM_NODES} \
+  NAMESPACE=${NS} \
+  APPLICATION_NAME=${APP} \
+  APPLICATION_USER=${USR} \
+  APPLICATION_PASSWORD=${PASS} | oc create -f -
+
+# Deploy app
+cd app
+oc project myproject
+oc new-build --binary --name=app
+mvn clean package
+oc start-build app --from-dir=. --follow
+oc new-app app
+oc expose service app
+
+# TODO: Wait until curl returns success
+
+# Test
+curl http://app-myproject.127.0.0.1.nip.io/test
+
+# Inject
+curl http://app-myproject.127.0.0.1.nip.io/inject
+
+echo "Start dashboard"
