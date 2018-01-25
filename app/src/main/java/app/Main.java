ddf9167c536ff2babcf962c5b02bd9c1fa8157bc
Galder Zamarreño <galder@zamarreno.com>
2018-01-25 15:51:41 +0100

    Use RxJava 2 and more idiomatic code for initial example

--- app/src/main/java/app/Main.java
+++ app/src/main/java/app/Main.java
@@ -1,27 +1,71 @@
 package app;
 
-import io.vertx.core.AbstractVerticle;
-import io.vertx.core.DeploymentOptions;
-import io.vertx.core.Future;
-import io.vertx.core.http.HttpHeaders;
-import io.vertx.core.json.JsonObject;
-import io.vertx.ext.web.Router;
-import io.vertx.ext.web.RoutingContext;
+import hu.akarnokd.rxjava2.interop.CompletableInterop;
+import io.reactivex.Single;
+import io.vertx.core.Handler;
+import io.vertx.reactivex.core.AbstractVerticle;
+import io.vertx.reactivex.core.Future;
+import io.vertx.reactivex.ext.web.Router;
+import io.vertx.reactivex.ext.web.RoutingContext;
 import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 
-import java.net.SocketAddress;
-import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import static io.reactivex.Single.just;
 
 public class Main extends AbstractVerticle {
 
   static final Logger log = Logger.getLogger(Main.class.getName());
 
   @Override
-  public void start(Future<Void> startFuture) throws Exception {
-    // TODO - live code
+  public void start(io.vertx.core.Future<Void> future) throws Exception {
+    Router router = Router.router(vertx);
+    router.get("/test").handler(this::test);
+
+    vertx.createHttpServer()
+      .requestHandler(router::accept)
+      .rxListen(8080)
+      .subscribe(
+        server -> {
+          log.info("HTTP server started");
+          future.complete();
+        },
+        failure -> {
+          log.log(Level.SEVERE, "HTTP server failed to start", failure);
+          future.fail(failure);
+        }
+      );
+  }
+
+  private void test(RoutingContext rc) {
+    vertx
+      .rxExecuteBlocking(remoteCacheManager())
+      .flatMap(remote -> vertx.rxExecuteBlocking(remoteCache(remote)))
+      .flatMap(cache -> CompletableInterop.fromFuture(cache.putAsync("hello", "world")).andThen(just(cache)))
+      .flatMap(cache -> Single.fromFuture(cache.getAsync("hello")))
+      .subscribe(
+        value ->
+          rc.response().end(value)
+        , failure ->
+          rc.response().end("Failure: " + failure.toString())
+      )
+    ;
+  }
+
+  private static Handler<Future<RemoteCacheManager>> remoteCacheManager() {
+    return f -> f.complete(
+      new RemoteCacheManager(
+        new ConfigurationBuilder().addServer()
+          .host("datagrid-hotrod")
+          .port(11222)
+          .build()));
+  }
+
+  private static Handler<Future<RemoteCache<String, String>>> remoteCache(RemoteCacheManager remote) {
+    return f -> f.complete(remote.getCache("repl"));
   }
 
 }
