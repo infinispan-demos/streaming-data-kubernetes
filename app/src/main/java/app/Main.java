be601040c2b58d71c9601ef946047e0ffcf77186
Galder Zamarreño <galder@zamarreno.com>
2018-01-26 11:30:35 +0100

    Convert Main back to being ready for live coding

--- app/src/main/java/app/Main.java
+++ app/src/main/java/app/Main.java
@@ -12,7 +12,6 @@ import org.infinispan.client.hotrod.RemoteCache;
 import org.infinispan.client.hotrod.RemoteCacheManager;
 import org.infinispan.client.hotrod.configuration.ConfigurationBuilder;
 
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import static io.reactivex.Single.just;
@@ -23,47 +22,9 @@ public class Main extends AbstractVerticle {
 
   @Override
   public void start(io.vertx.core.Future<Void> future) {
-    Router router = Router.router(vertx);
-    router.get("/test").handler(this::test);
-
-    vertx.createHttpServer()
-      .requestHandler(router::accept)
-      .rxListen(8080)
-      .subscribe(
-        server -> {
-          log.info("HTTP server started");
-          future.complete();
-        },
-        future::fail
-      );
+    // TODO - live code
   }
 
-  private void test(RoutingContext rc) {
-    vertx
-      .rxExecuteBlocking(Main::remoteCacheManager)
-      .flatMap(remote -> vertx.rxExecuteBlocking(remoteCache(remote)))
-      .flatMap(cache -> CompletableInterop.fromFuture(cache.putAsync("hello", "world")).andThen(just(cache)))
-      .flatMap(cache -> Single.fromFuture(cache.getAsync("hello")))
-      .subscribe(
-        value ->
-          rc.response().end(value)
-        , failure ->
-          rc.response().end("Failure: " + failure.toString())
-      )
-    ;
-  }
-
-  private static void remoteCacheManager(Future<RemoteCacheManager> f) {
-    f.complete(
-      new RemoteCacheManager(
-        new ConfigurationBuilder().addServer()
-          .host("datagrid-hotrod")
-          .port(11222)
-          .build()));
-  }
-
-  private static Handler<Future<RemoteCache<String, String>>> remoteCache(RemoteCacheManager remote) {
-    return f -> f.complete(remote.getCache("repl"));
-  }
+  // TODO - live code
 
 }
